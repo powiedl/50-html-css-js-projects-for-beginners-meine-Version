2a8b6523772ac199f0afd76a1835be0a0d2a50ac
powiedl <159603185+powiedl@users.noreply.github.com>
2024-03-10 14:21:49 +0100

    in die :before auch die Einzahlberücksichtigung eingebaut (mit CSS-Variablen)

--- 3-new-year-countdown/script.js
+++ 3-new-year-countdown/script.js
@@ -1,6 +1,7 @@
 const templateText='${days} Days&nbsp;&nbsp;&nbsp;${hours} Hours&nbsp;&nbsp;&nbsp;${minutes} Minutes&nbsp;&nbsp;&nbsp;${seconds} Seconds'
 const spanCountdown=document.querySelector('#spanCountdown')
 const spanNewYear=document.querySelector('#spanNewYear')
+const root = document.querySelector(":root"); // root Element in CSS
 
 function dateDifference (dateInitial, dateFinal) {
   let diff = (dateFinal - dateInitial) / 1000; // in seconds
@@ -19,6 +20,10 @@ function setCountdown() {
     spanNewYear.innerText = nextNewYear.getFullYear()
     const delta = dateDifference(new Date,nextNewYear);
     spanCountdown.innerHTML = `${delta.days} Day${delta.days !==1 && 's'}&nbsp;&nbsp;&nbsp;${delta.hours} Hour${delta.hours !==1 ? 's' :''}&nbsp;&nbsp;&nbsp;${delta.minutes} Minute${delta.minutes !==1 ? 's' :''}&nbsp;&nbsp;&nbsp;${delta.seconds} Second${delta.seconds !==1 ? 's' :''}`
+    delta.days === 1 ? root.style.setProperty("--day",`"DAY"`) : root.style.setProperty("--day",`"DAYS"`)
+    delta.hours === 1 ? root.style.setProperty("--hour",`"HOUR"`) : root.style.setProperty("--hour",`"HOURS"`)
+    delta.minutes === 1 ? root.style.setProperty("--minute",`"MINUTE"`) : root.style.setProperty("--minute",`"MINUTES"`)
+    delta.seconds === 1 ? root.style.setProperty("--second",`"SECOND"`) : root.style.setProperty("--second",`"SECONDS"`)
     document.querySelector('#day').innerText=delta.days
     document.querySelector('#hour').innerText=delta.hours
     document.querySelector('#minute').innerText=delta.minutes
